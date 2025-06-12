dde1a23fd8dfa526e253be1b6374e5f9bb5c1c57
Priyanshu Prasad Gupta <priyanshoon.pg@gmail.com>
2025-06-13 00:55:52 +0530

    update: swtich cases, if else, else if etc...

--- basics/main.go
+++ basics/main.go
@@ -22,4 +22,53 @@ func main() {
 		}
 	*/
 
+	// switch cases condition things
+
+	/*
+		switch expression {
+		case value1:
+			// code
+			fallthrough // this does not break and hence continue
+		case value2:
+			// code
+		default:
+			// code
+		}
+
+		here is the catch, unlike other language go break automatically when case statisfied, If one want to continue executing below then use "fallthrough"
+	*/
+
+	fruit := "apple"
+
+	switch fruit {
+	case "apple":
+		fmt.Println("Booyaah")
+		fallthrough
+	case "banana":
+		fmt.Println("Nooyaah")
+	default:
+		fmt.Println("Mmmyaah")
+	}
+
+	day := "sunday"
+
+	switch day {
+	case "monday", "tuesday", "wednesday", "thursday", "friday":
+		fmt.Println("Weekdays")
+	case "sunday", "saturday":
+		fmt.Println("Weekends")
+	default:
+		fmt.Println("invalid day")
+	}
+
+	number := 15
+
+	switch {
+	case number < 10:
+		fmt.Println("number is less than 10")
+	case number >= 10 && number < 20:
+		fmt.Println("Number is between 10 and 20")
+	default:
+		fmt.Println("Number is greater than 20")
+	}
 }
