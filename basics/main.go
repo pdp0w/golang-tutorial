a067648f5152a1284aeaa211685357a39d509a9d
Priyanshu Prasad Gupta <priyanshoon.pg@gmail.com>
2025-06-13 00:16:46 +0530

    intial commit

---
+++ basics/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	// conditional statement
+	if 5 == 5 {
+		fmt.Println(true)
+	} else {
+		fmt.Println(false)
+	} // same as c
+
+	/*
+		if (condition) {
+			body...
+		} else if (condition) {
+			body...
+		} else {
+			body...
+		}
+	*/
+
+}
