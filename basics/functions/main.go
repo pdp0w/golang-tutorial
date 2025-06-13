8f367f8ca09229c1c4bf777e047c5c8e63dadec4
Priyanshu Prasad Gupta <priyanshoon.pg@gmail.com>
2025-06-13 13:58:36 +0530

    update: function completed

--- basics/functions/main.go
+++ basics/functions/main.go
@@ -23,8 +23,24 @@ func main() {
 	}
 
 	greet()
+
+	result := applyOperation(5, 3, add)
+	fmt.Println(result)
+
+	multiplyBy2 := createMultiplier(2)
+	fmt.Println(multiplyBy2(6))
 }
 
 func add(a, b int) int {
 	return a + b
+}
+
+func applyOperation(x, y int, operation func(int, int) int) int {
+	return operation(x, y)
+}
+
+func createMultiplier(factor int) func(int) int {
+	return func(x int) int {
+		return factor * x
+	}
 }
